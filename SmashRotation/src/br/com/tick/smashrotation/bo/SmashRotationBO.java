c75c69ee36b805ed5acb672be1cfe64e6f6c75ba
TiagoDvl <tdavilas@gmail.com>
2015-02-27 01:06:49 -0400

    BO++
    O objeto vai ser utilizado no projeto inteiro. Melhor deixá-lo global e
    fazer getter/setter pra lista.

--- SmashRotation/src/br/com/tick/smashrotation/bo/SmashRotationBO.java
+++ SmashRotation/src/br/com/tick/smashrotation/bo/SmashRotationBO.java
@@ -9,9 +9,10 @@ import br.com.tick.smashrotation.domain.Player;
 public class SmashRotationBO {
 
 	private static SmashRotationBO instance = null;
+	private List<Player> listOfPlayers;
 
 	protected SmashRotationBO(Context context) {
-		// Do Nothing.
+		listOfPlayers = new ArrayList<Player>();
 	}
 
 	public static SmashRotationBO getInstance(Context context) {
@@ -22,40 +23,52 @@ public class SmashRotationBO {
 	}
 	
 	public List<Player> generateStaticPlayers() {
-		List<Player> listOfPlayers = new ArrayList<Player>();
 		Player currentPlayer = new Player();
 		
 		currentPlayer.setName("Tikun");
 		currentPlayer.setWins(10);
 		currentPlayer.setLosses(5);
-		listOfPlayers.add(currentPlayer);
+		getListOfPlayers().add(currentPlayer);
 		
 		currentPlayer.setName("nRick");
 		currentPlayer.setWins(1);
 		currentPlayer.setLosses(5000);
-		listOfPlayers.add(currentPlayer);
+		getListOfPlayers().add(currentPlayer);
 		
 		currentPlayer.setName("Lucas");
 		currentPlayer.setWins(50);
 		currentPlayer.setLosses(10);
-		listOfPlayers.add(currentPlayer);
+		getListOfPlayers().add(currentPlayer);
 		
 		currentPlayer.setName("Fabio");
 		currentPlayer.setWins(20);
 		currentPlayer.setLosses(10);
-		listOfPlayers.add(currentPlayer);
+		getListOfPlayers().add(currentPlayer);
 		
 		currentPlayer.setName("Ramon");
 		currentPlayer.setWins(10);
 		currentPlayer.setLosses(2);
-		listOfPlayers.add(currentPlayer);
+		getListOfPlayers().add(currentPlayer);
 		
 		currentPlayer.setName("Rendom");
 		currentPlayer.setWins(0);
 		currentPlayer.setLosses(2);
-		listOfPlayers.add(currentPlayer);
+		getListOfPlayers().add(currentPlayer);
 
+		return getListOfPlayers();
+	}
+
+	public List<Player> getListOfPlayers() {
 		return listOfPlayers;
 	}
 
+	public void setListOfPlayers(List<Player> listOfPlayers) {
+		this.listOfPlayers = listOfPlayers;
+	}
+	
+	public void defineWinner() {
+		// TODO Auto-generated method stub
+
+	}
+	
 }
